5b8cc1fbce86c245b217b98022774cc43c8514e6
heiligbasil <basilief@yahoo.com>
2019-05-27 14:59:35 -0700

    First processing directive complete

--- src/Main.kt
+++ src/Main.kt
@@ -4,9 +4,13 @@ fun main(args: Array<String>) {
     var vending1:Vending=Vending(101,"Food")
     var vending2:Vending=Vending(102,"Drink")
     var vending3:Vending=Vending(103,"Office")
-    var chips:Snack=Snack(1001,"Chips",36,1.75,101)
-    var chocolates:Snack=Snack(1002,"Chocolate Bar",36,1.00,101)
-    var pretzels:Snack=Snack(1003,"Pretzels",30,2.00,101)
-    var soda:Snack=Snack(1004,"Soda",24,2.50,102)
-    var water:Snack=Snack(1005,"Water",20,2.75,102)
+    var chips:Snack=Snack(1001,"Chips",36,1.75,vending1)
+    var chocolates:Snack=Snack(1002,"Chocolate Bar",36,1.00,vending1)
+    var pretzels:Snack=Snack(1003,"Pretzels",30,2.00,vending1)
+    var soda:Snack=Snack(1004,"Soda",24,2.50,vending2)
+    var water:Snack=Snack(1005,"Water",20,2.75,vending2)
+
+    customer1.buySnack(soda.buySnack(3))
+    println(customer1.cash)
+    println(soda.getQuantity())
 }
--- src/Snack.kt
+++ src/Snack.kt
@@ -1,11 +1,16 @@
-class Snack(val id: Int, val name: String, private var quantity: Int, var cost: Double, val vendingId: Int) {
+class Snack(val id: Int, val name: String, private var quantity: Int, var cost: Double, val vendingId: Vending) {
 
-    fun addQuantity(quantity: Int) {
+    fun addSnack(quantity: Int) {
         this.quantity += quantity
     }
 
     fun buySnack(quantity: Int): Double {
+        this.quantity=this.quantity.minus(quantity)
         return quantity.times(cost)
+    }
+
+    fun getQuantity():Int {
+        return this.quantity
     }
 
     fun getTotalCost(quantity: Int): Double {
